cb98217c78305a544b0108fb79504e40b5e98529
Yasin Keyani <ykeyani@gmail.com>
2021-12-05 17:34:24 +0000

    Simplify model
    remove irrelevant code

--- api/polls/admin.py
+++ api/polls/admin.py
@@ -16,7 +16,7 @@ class QuestionAdmin(admin.ModelAdmin):
 
     inlines = (ChoiceInline,)
 
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
--- api/polls/models.py
+++ api/polls/models.py
@@ -1,8 +1,4 @@
-import datetime
-
-from django.contrib import admin
 from django.db import models
-from django.utils import timezone
 
 
 class Question(models.Model):
@@ -11,14 +7,6 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
